54e9f2550dfc124d8f8f80a699736b08a4bdb143
synasapmob <sonn51280@gmail.com>
2026-06-25 13:15:07 +0700

    Format prettier to consistency

--- frontend/src/games/regularPayments/components/SketchDefs.tsx
+++ frontend/src/games/regularPayments/components/SketchDefs.tsx
@@ -21,23 +21,23 @@ export function SketchDefs(): JSX.Element {
         }
       `}</style>
       <svg width="0" height="0" className="absolute size-0">
-      <filter id="pshopRough" x="-6%" y="-6%" width="112%" height="112%">
-        <feTurbulence
-          type="fractalNoise"
-          baseFrequency="0.018"
-          numOctaves={2}
-          seed={7}
-          result="noise"
-        />
-        <feDisplacementMap
-          in="SourceGraphic"
-          in2="noise"
-          scale="2.6"
-          xChannelSelector="R"
-          yChannelSelector="G"
-        />
-      </filter>
-    </svg>
+        <filter id="pshopRough" x="-6%" y="-6%" width="112%" height="112%">
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="0.018"
+            numOctaves={2}
+            seed={7}
+            result="noise"
+          />
+          <feDisplacementMap
+            in="SourceGraphic"
+            in2="noise"
+            scale="2.6"
+            xChannelSelector="R"
+            yChannelSelector="G"
+          />
+        </filter>
+      </svg>
     </>
   );
 }
